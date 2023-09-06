b1fe483f21f5ba5fdccd3a7981d6597224fe2fd8
Tanya <krajnikova2004@gmail.com>
2023-09-06 16:54:43 +0100

    Update functionality

--- school-manager-server/src/main/java/com/hillel/multi/presentation/controller/StudentController.java
+++ school-manager-server/src/main/java/com/hillel/multi/presentation/controller/StudentController.java
@@ -1,13 +1,11 @@
 package com.hillel.multi.presentation.controller;
 
-
-import com.hillel.api.SchoolManagerApi;
-import com.hillel.model.Student;
+import com.hillel.multi.persistent.entity.Student;
 import com.hillel.multi.service.StudentsService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -18,59 +16,43 @@ import java.util.Map;
 @RestController
 @RequestMapping(path = "/api/students")
 @Validated
-public class StudentController implements SchoolManagerApi {
+public class StudentController {
 
     @Autowired
     StudentsService studentsService;
 
-    @Override
-    public ResponseEntity<Student> getStudentById(String id, String verbosity) {
-        return SchoolManagerApi.super.getStudentById(id, verbosity);
+    @GetMapping("/all")
+    public @ResponseBody List<Student> getAllStudents() {
+        return studentsService.getAllStudents();
     }
 
-    @Override
-    public ResponseEntity<Student> postStudent(Student student) {
-        return SchoolManagerApi.super.postStudent(student);
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/{id}",
+            produces = {"application/json"}
+    )
+    public ResponseEntity<Student> getStudentById(@PathVariable("id") int id,
+                                                   @RequestParam(value = "verbosity",
+                                                   required = false,
+                                                   defaultValue = "all") String verbosity) {
+        return studentsService.getStudentById(id);
     }
 
-    @Override
-    public ResponseEntity<Student> putStudent(String id, Student student, String language) {
-        return SchoolManagerApi.super.putStudent(id, student, language);
+    @PutMapping(value = "/{id}")
+    public Student updateStudent(@PathVariable int id,
+                                 @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE, required = false) String language,
+                                 @RequestHeader Map<String, String> headers,
+                                 @RequestBody Student student) {
+        return studentsService.updateStudent(student);
     }
 
+    @PostMapping("/add")
+    public Student addStudent(@Valid @RequestBody(required = false) Student student) {
+        return studentsService.addStudent(student);
+    }
 
-//    @GetMapping("/all")
-//    public @ResponseBody List<Student> getAllStudents() {
-//        return studentsService.getAllStudents();
-//    }
-//
-//    @RequestMapping(
-//            method = RequestMethod.GET,
-//            value = "/{id}",
-//            produces = {"application/json"}
-//    )
-//    public ResponseEntity<Student> getStudentById(@PathVariable("id") int id,
-//                                                   @RequestParam(value = "verbosity",
-//                                                   required = false,
-//                                                   defaultValue = "all") String verbosity) {
-//        return studentsService.getStudentById(id);
-//    }
-//
-//    @PutMapping(value = "/{id}")
-//    public Student updateStudent(@PathVariable int id,
-//                                 @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE, required = false) String language,
-//                                 @RequestHeader Map<String, String> headers,
-//                                 @RequestBody Student student) {
-//        return studentsService.updateStudent(student);
-//    }
-//
-//    @PostMapping("/add")
-//    public Student addStudent(@Valid @RequestBody(required = false) Student student) {
-//        return studentsService.addStudent(student);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void deleteStudentById(@PathVariable("id") int id) {
-//        studentsService.deleteStudentById(id);
-//    }
+    @DeleteMapping("/{id}")
+    public void deleteStudentById(@PathVariable("id") int id) {
+        studentsService.deleteStudentById(id);
+    }
 }
